ed6d1a0dadb4f94a24250f011bcfcc1820b3ef40
CS17B011 <cs17b011@iittp.ac.in>
2019-10-04 14:04:27 +0530

    Starter Code

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv/config')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT =  process.env.PORT || 4000;
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.DBURL, {useNewUrlParser: true, useCreateIndex: true})
+    .then(() => console.log('Database is Connected...'))
+    .catch((err) => console.log(err));
+
+app.get('/',(req,res) => {
+	res.send("<h1>Tirutsava 2020 Website Backend</h1>");
+});
+
+app.listen(PORT, function(){
+  console.log('Server is running on Port: ',PORT);
+});
