de1a4308aecf2ee15a9fcee51720a37a4c5a0e32
chirag2706 <43676985+chirag2706@users.noreply.github.com>
2019-11-15 15:04:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,30 +5,23 @@ const bodyParser = require('body-parser');
 const PORT =  process.env.PORT || 4000;
 const cors = require('cors');
 const path = require('path');
-const passport = require('passport');
-const users = require('./routes/users');
 const mongoose = require('mongoose');
-
+const questionAns = require('./routes/questionAns');
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.DBURL, {useNewUrlParser: true, useCreateIndex: true})
+app.use('/adminPanel',questionAns);
+
+mongoose.connect('mongodb://localhost:27017/c3', {useUnifiedTopology: true,useNewUrlParser: true, useCreateIndex: true})
     .then(() => console.log('Database is Connected...'))
     .catch((err) => console.log(err));
-
-//Passport Middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./config/passport')(passport);
-
-app.use('/users', users);
-
 
 app.get('/',(req,res) => {
 	res.send("<h1>Tirutsava 2020 Website Backend</h1>");
 });
+
+// app.post('/')
 
 app.listen(PORT, function(){
   console.log('Server is running on Port: ',PORT);
