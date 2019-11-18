92fa091ad0d9587d997384e4b8f941b86d03e3f7
me17b016 <rajgarg383@gmail.com>
2019-11-18 19:13:03 +0530

    landing page

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,10 @@ import './pages/sponsers/sponsers.component';
 import * as serviceWorker from './serviceWorker';
 import Sponsers from './pages/sponsers/sponsers.component';
 import CrossPuzzle from './pages/crosspuzzle/crossmain.component';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavPage from "./pages/landingpage/navpage/navepage.component";
 
-ReactDOM.render(<CrossPuzzle/>, document.getElementById('root'));
+ReactDOM.render(<Router><NavPage/></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
